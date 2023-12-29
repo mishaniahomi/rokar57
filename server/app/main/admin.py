bf0e6f1be6c1f59db898a89350e6a9884973f7a9
mishaniahomi <misha.homi@gmail.com>
2023-12-29 11:41:42 +0300

    Add banner model and banner admin

--- server/app/main/admin.py
+++ server/app/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+@admin.register(models.Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'created_at', 'href', 'prewiew')
+    list_display_links = ('id', 'title', 'description', 'created_at', 'href', 'prewiew')
+    search_fields = ('id', 'title', 'description', 'created_at', 'href')
+
+    def prewiew(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-width: 70%;">')
+
--- server/app/main/models.py
+++ server/app/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Banner(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Заголовок баннера')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name="Изображение баннера", upload_to='banner_image/')
+    created_at = models.DateField(default=datetime.date.today, verbose_name='Дата создания')
+    href = models.TextField(verbose_name="Ссылка", blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Банер'
+        verbose_name_plural = 'Банеры'
+
+    def __str__(self) -> str:
+        return self.title
